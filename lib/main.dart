367e33fb4194bdb7be2384b892a69dacd3fdcde7
GeniuStone <dugustlr@naver.com>
2023-01-25 17:23:22 +0900

    박스 디자인 하는 법

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,20 +18,44 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('앱임')),
-        body: Text('안녕 ㅋ'),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 60,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-            ),
-          )
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 100, height: 100, color: Colors.orange, margin: EdgeInsets.all(20), padding: EdgeInsets.all(20),
+                child: Text('박스 1'),
+              ),
+              Container(
+                width: 120, height: 120, color: Colors.orange, margin: EdgeInsets.fromLTRB(20, 20, 20, 20), padding: EdgeInsets.fromLTRB(10, 15, 10, 15),
+                child: Text('박스 2'),
+              ),
+              Container(
+                width: 140, height: 140, margin: EdgeInsets.all(20), padding: EdgeInsets.all(5),
+                child: Text('기타 자잘한 스타일 설정은 decoration에다가'),
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  border: Border.all(color: Colors.indigo, width: 10),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Text("Align 위젯으로도 정렬할 수 있음"),
+              )
+            ],
+          ),
         ),
+
       ),
     );
   }
